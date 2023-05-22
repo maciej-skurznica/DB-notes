d8daea2e9ce3a561a3f26e1be5fd52dba70e5ae2
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-05-22 18:21:43 +0100

    / page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="h-screen">
-      Home
+      Home | login to see your notes and go to the notes page
       <Link href="/notes">Notes</Link>
     </main>
   );
